42bc222a5a06df19a8bdb2efa34a06447a67b02a
sai ganesh <saiganesh@sais-MacBook-Pro.local>
2021-02-16 07:12:59 -0500

    Header & Footer Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Container, Row } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <main>
+        <Container>
+          <Row>
+            <h1 className='text-center'>Sello MarketPlace</h1>
+          </Row>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App

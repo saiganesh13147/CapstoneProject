8df25467aa8b2572f36dfa201a47ea3ef4d3b388
sai ganesh <saiganesh@sais-MacBook-Pro.local>
2021-03-12 09:27:44 -0500

    Fixed Errors

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,24 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
 
 const ProductScreen = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id)
+  const [product, setProduct] = useState({})
 
-  //   const [products, setProducts] = useState([])
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [])
 
-  //   useEffect(() => {
-  //     const fetchProducts = async () => {
-  //       const { data } = await axios.get('api/products')
-
-  //       setProducts(data)
-  //     }
-  //     fetchProducts()
-  //   }, [])
-
-  console.log(product)
   return (
     <div>
       <LinkContainer to='/'>
@@ -42,6 +38,33 @@ const ProductScreen = ({ match }) => {
             <ListGroup.Item>Price : ${product.price}</ListGroup.Item>
             <ListGroup.Item>Description : {product.description}</ListGroup.Item>
           </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button className='btn-block' type='button'>
+                  Add to Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
         </Col>
       </Row>
     </div>
